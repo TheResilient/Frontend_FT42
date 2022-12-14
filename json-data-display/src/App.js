5567af54cd15304e036ae8f1d8f46da96be2288a
TheResilient <pratikphadtare109@gmail.com>
2022-12-14 14:58:38 +0530

    added page7

--- json-data-display/src/App.js
+++ json-data-display/src/App.js
@@ -5,61 +5,65 @@ import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import FormControl, { useFormControl } from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import Box from "@mui/material/Box";
+// import { makeStyles } from "tss-react/mui";
+
+// const useTableHeaderStyles = makeStyles({
+//   // .MuiTableHead-root
+//   root: {
+//     borderRadius: "10px",
+//   },
+// });
 
 function App() {
   // var [selects, setSelects] = useState();
   // console.log(selects);
+  // const tableHeaderClasses = useTableHeaderStyles();
   return (
     <div className="App">
       <div className="posts">
         <div className="head1">
           <h3>Asset Details</h3>
-          {data.map((item, i) =>
-            {return (
+          {data.map((item, i) => {
+            return (
               <div key={i}>
                 {item.name}
-                <br></br>
-                <br></br>
+                <pre />
                 {item.type}
-                <br></br>
-                <br></br>
+                <pre />
                 <FormControl label={'margin="none"'} sx={{ width: "65ch" }}>
                   <OutlinedInput defaultValue={item.details} />
                 </FormControl>
-                <br></br>
-                <br></br>
-                Attributes:<br></br>
-                <br></br>
-                <TableContainer component={Paper}>
-                  <Table sx={{ maxWidth: 560 }} aria-label="simple table">
-                    <TableHead>
+                <pre />
+                <b>Attributes</b>
+                <pre />
+                <Table
+                  sx={{ maxWidth: "650px", borderRadius: "106px" }}
+                  aria-label="simple table"
+                >
+                  <TableHead>
+                    {/* <TableHead classes={tableHeaderClasses.root}> */}
+                    <TableRow>
+                      <TableCell>Key</TableCell>
+                      <TableCell>Value</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {item.attributes.map((sub) => (
                       <TableRow>
-                        <TableCell>Key</TableCell>
-                        <TableCell>Value</TableCell>
+                        <TableCell>{sub.key}</TableCell>
+                        <TableCell>{sub.value}</TableCell>
                       </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {item.attributes.map((sub) => (
-                        <TableRow>
-                          <TableCell>{sub.key}</TableCell>
-                          <TableCell>{sub.value}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+                    ))}
+                  </TableBody>
+                </Table>
               </div>
             );
           })}
         </div>
-
 
         {/* <div>
           <select defaultValue={selects} value={selects} onChange={(e) => setSelects(e.target.value)}>
@@ -100,6 +104,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
