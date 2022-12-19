346ff0a0abab5142549ed480fbbeb8b67972ba50
TheResilient <pratikphadtare109@gmail.com>
2022-12-19 23:10:47 +0530

    added final commit

--- json-data-display/src/App.js
+++ json-data-display/src/App.js
@@ -2,8 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import data from "./data.json";
 import React from "react";
-import FormControl from "@mui/material/FormControl";
-import OutlinedInput from "@mui/material/OutlinedInput";
 import { Box } from "@mui/material";
 
 function App() {
@@ -19,9 +17,7 @@ function App() {
                 <pre />
                 {item.type}
                 <pre />
-                {/* <FormControl label={'margin="none"'} sx={{ width: "65ch" }}>
-                  <OutlinedInput defaultValue={item.details} />
-                </FormControl> */}
+                
                 <Box
                   sx={{
                     width: 300,
@@ -35,26 +31,7 @@ function App() {
                 <pre />
                 <h4>Attributes</h4>
                 <pre />
-                {/* <Table
-                  sx={{ maxWidth: "650px", borderRadius: "106px" }}
-                  aria-label="simple table"
-                >
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Key</TableCell>
-                      <TableCell>Value</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {item.attributes.map((sub) => (
-                      <TableRow>
-                        <TableCell>{sub.key}</TableCell>
-                        <TableCell>{sub.value}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table> */}
-
+                
                 <table>
                   <tr>
                     <th class="ars">Key</th>
@@ -73,41 +50,6 @@ function App() {
           })}
         </div>
 
-        {/* <div>
-          <select defaultValue={selects} value={selects} onChange={(e) => setSelects(e.target.value)}>
-            
-            {data.map((result) => (
-              <option value={result.name}>{result.name}</option>
-            ))}
-          </select>
-        </div>
-
-        <br></br>
-        <br></br>
-        <table>
-          <thead>
-              <th>Key</th>
-              <th>Value</th>
-          </thead>
-          {data
-            .filter((data) => data.name == selects)
-            .map((data) => {
-              return (
-                <tr>
-                  <td>
-                    {Object.keys(data).map((key, index) => {
-                      return <tr key={index}>{key}</tr>; 
-                    })}
-                  </td>
-                  <td>
-                    {Object.values(data).map((value, index) => {
-                      return <tr key={index}>{value}</tr>;
-                    })}
-                  </td>
-                </tr>
-              );
-            })}
-        </table> */}
       </div>
     </div>
   );
